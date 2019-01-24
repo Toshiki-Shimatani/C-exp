d6764383988da428505641f2e21c325edaa56226
Toshiki Shimatani <p3050rhk@s.okayama-u.ac.jp>
2019-01-25 02:14:14 +0900

    OK

---
+++ compiler/spim/bin/a.s
@@ -0,0 +1,18 @@
+	.file	1 "a.c"
+
+ # -G value = 0, Cpu = r2000, ISA = 1
+ # GNU C version 2.96-mips3264-000710 (mipsel-linux) compiled by GNU C version 2.96 20000731 (Red Hat Linux 7.2 2.96-112.7.2).
+ # [AL 1.1, MM 40] BSD Mips
+ # options passed:  -mno-abicalls -mrnames -mmips-as
+ # -mno-check-zero-division -mcpu=r2000 -O0 -fleading-underscore
+ # -finhibit-size-directive -fverbose-asm
+ # options enabled:  -fpeephole -fkeep-static-consts -fpcc-struct-return
+ # -fsched-interblock -fsched-spec -fnew-exceptions -fcommon
+ # -finhibit-size-directive -fverbose-asm -fgnu-linker -flive-range-gdb
+ # -fargument-alias -fleading-underscore -fdelay-postincrement -fident
+ # -fmath-errno -msplit-addresses -mrnames -mno-check-zero-division
+ # -mdebugf -mdebugi -mno-div-checks -mcpu=r2000
+
+
+
+	.comm	_a,4
